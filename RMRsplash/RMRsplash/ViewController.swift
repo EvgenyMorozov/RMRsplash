3683b2cfed60ef97f0f478c8a93ec364c29af33f
morozov <morozov@surfstudio.ru>
2020-01-04 14:58:54 +0300

    Create project

---
+++ RMRsplash/RMRsplash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RMRsplash
+//
+//  Created by Evgeny Morozov on 04/01/2020.
+//  Copyright © 2020 EvgenyMorozov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
